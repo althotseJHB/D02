3e14317907d8adbf62652022c37a46739569feeb
althotseJHB <althotse@student.wethinkcode.co.za>
2020-07-09 13:36:12 +0200

    Completed exercises

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
 
+void ft_putchar(char alpha){
+  write(1,&alpha,1);
+}
+
 void ft_print_alphabet(void)
 {
   char alpha;
   alpha = 'a';
   while (alpha <= 'z')
   {
-    write(1,&alpha,1);
+    // write(1,&alpha,1); "could also be written here and neglect ft_putchar"
+    ft_putchar(alpha);
     alpha++;
   }
 }
--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
+void ft_putchar(char alpha){
+  write(1,&alpha,1);
+}
+
 void ft_print_alphabet(void)
 {
   char alpha;
   alpha = 'z';
   while (alpha >= 'a')
   {
-    write(1,&alpha,1);
+    // write(1,&alpha,1);
+    ft_putchar(alpha);
     alpha--;
   }
 }
