4348c01eccd7528485e8466b882e5a2687b8bcb9
Alex Thotse <althotse@c4r21s5.wethinkcode.co.za>
2020-07-10 09:55:02 +0200

    Testing ft_print_comb exercise

---
+++ ex04/ft_print_comb.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+  write(1,&c,1);
+}
+void ft_print_comb(void)
+{
+  char a = '0';
+  char b = '0';
+  char c = '0';
+
+    while (a <= 9)
+    {
+      while (b <= 9)
+      {
+        while (c <= 9) {
+          ft_putchar(c);
+          c++
+        }
+        ft_putchar(b);
+        b++
+      }
+    ft_putchar(a);
+    a++
+    }
+}
+
+int main() {
+  ft_print_comb();
+}
