f3ee9f4e6024041dcd8778076525f44d422ddaef
Alex Thotse <althotse@c4r21s5.wethinkcode.co.za>
2020-07-15 13:57:13 +0200

    completed exercise 04

--- ex04/ft_print_comb.c
+++ ex04/ft_print_comb.c
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-/*#include <unistd.h>
-=======
 #include <unistd.h>
->>>>>>> 0a209d48e47b1848d32f921a89f10a67e0c37678
 
 void ft_putchar(char c)
 {
   write(1,&c,1);
-<<<<<<< HEAD
-
-
-
-
 }
 void ft_print_numb(char a, char b, char c)
 {
@@ -22,19 +13,13 @@ void ft_print_numb(char a, char b, char c)
       ft_putchar(' ');
 }
 
-
-
-
-=======
 }
->>>>>>> 0a209d48e47b1848d32f921a89f10a67e0c37678
 void ft_print_comb(void)
 {
   char a = '0';
   char b = '0';
   char c = '0';
 
-<<<<<<< HEAD
     while (a < '9')
     {
       while (b < '9')
@@ -47,8 +32,7 @@ void ft_print_comb(void)
     c = 0;
 	b++;
       }
-	b=0;   
-=======
+	b=0;
     while (a <= '9')
     {
       while (b <= '9')
@@ -61,68 +45,12 @@ void ft_print_comb(void)
         b++;
       }
     ft_putchar(a);
->>>>>>> 0a209d48e47b1848d32f921a89f10a67e0c37678
     a++;
     }
 }
 
-int main() {
+int main()
+{
   ft_print_comb();
   return 0;
 }
-<<<<<<< HEAD
-*/
-
-#include <unistd.h>
-
-void ft_putchar(char c)
-{
-	  write(1,&c,1);
-}
-
-void ft_print_numb(char a, char b, char c)
-{
-	  
-	  ft_putchar(a);
-	  ft_putchar(b);
-	  ft_putchar(c);
-	  ft_putchar(',');
-	  ft_putchar(' ');
-}
-
-void ft_print_comb(void) 
-{
-	  char a = '0';
-	    char b = '0';
-		  char c = '0';
-
-		    while (a <=  '9')
-				  {
-				    while(b <=  '9')
-				    {
-					  while(c <= '9')
-					  {
-					  //  if(a < b && b<c)
-					//	{
-						 	 ft_print_numb(a,b,c);
-					//	}
-						c++;
-													        
-					 }
-					 c = '0';
-					 b++;
-				    }
-					b = '0';
-					a++;
-				  }
-
-}
-
-int main()
-{
-	  ft_print_comb();
-	    return 0;
-}
-
-=======
->>>>>>> 0a209d48e47b1848d32f921a89f10a67e0c37678
