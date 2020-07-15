d1908d5f3d5beffe6560ee0a6a2baca8e396cb5c
Alex Thotse <althotse@c4r21s5.wethinkcode.co.za>
2020-07-15 15:30:31 +0200

    completed ft_print_number exercise

--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -10,17 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include <unistd.h>
-
 void ft_putchar(char number){
-  write(1,&number,1);
+
 }
 
 void ft_print_number(void)
 {
   char number;
   number = '0';
+  
   while (number <= '9')
   {
     ft_putchar(number);
